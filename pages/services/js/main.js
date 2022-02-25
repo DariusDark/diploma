56b83823f7bc9c04456ed8c08b8e57d5308c3548
DariusDark <darius99dark@mail.ru>
2022-02-25 08:47:28 +0500

    develop stage: done all content and styles

---
+++ pages/services/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('.toggle__title').click(function(event){
+		if ($('.toggle__item').hasClass('one')) {
+			$('.toggle__title').not($(this)).removeClass('active');
+			$('.toggle__text').not($(this).next()).slideUp(300);
+		}
+		$(this).toggleClass('active').next().slideToggle(300);
+	});
+});
